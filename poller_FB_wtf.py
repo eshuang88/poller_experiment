c99442c40ddb33c536e261ca9259bfab919def2e
eshuang88 <eshuang88@gmail.com>
2017-08-30 18:47:23 +0800

    renamed top5all to 5foreach

--- poller_FB_wtf.py
+++ poller_FB_wtf.py
@@ -6,12 +6,12 @@ import pandas as pd
 import ast
 import csv
 
-data = pd.read_csv('/Users/y/Desktop/Poller_experi/0804_FB_newsDeep.csv')
+data = pd.read_csv('/Users/y/Desktop/poller_dump/08190825_FB_food.csv')
 # merge column "message" and column "description" as a string
 # create a column named "allText"
 data = data.assign(
-	allText = (data['message'].astype(str) + data['description'].astype(str))
-	)
+    allText = (data['message'].astype(str) + data['description'].astype(str))
+    )
 
 # compute article popularity as L + C + 10S
 # find the last data point of likes/comments/shares
@@ -73,45 +73,38 @@ data = data.assign(
 
 #============term score calculation starts here================
 
-# text splitting using purewords
-texts = data['allText'].str.cat()
-texts_clean = pw.clean_sentence(texts)
-texts_list = texts_clean.split()
-word_count = dict(Counter(texts_list)) # using Counter to get term frequency
-word_count_sorted = sorted(word_count, key=word_count.get, reverse=True) # it's a list
-
 # create a customized term filter for Poller
 # DO NOT MESS WITH purewords/config/stopwords.text. Must create another filter.
 filter_word = pd.read_csv('/Users/y/Desktop/poller_experiment/poller_filter.txt', sep=",", header=None, engine='python')
 filter_words = filter_word[0].tolist() # is a list
 
-# create another list named word_count_clean
-# each item in word_count_clean is unique
-word_count_clean = []
-for i in word_count_sorted:
-    if i in filter_words:
+# text splitting using purewords
+texts = data['allText'].str.cat()
+texts_clean = pw.clean_sentence(texts)
+texts_list = texts_clean.split()
+
+text_count = []
+for text in texts_list:
+    if text in filter_words:
+        continue
+    elif len(text) == 1:
         continue
     else:
-        word_count_clean.append(i)
+        text_count.append(text)
 
-# get the simple term frequency
+word_count = dict(Counter(text_count)) # using Counter to get term frequency
+word_count_sorted = sorted(word_count, key=word_count.get, reverse=True) # it's a list
+
 '''
-term_tf_term = []
-term_tf_freq = []
-for i in word_count_clean:
-	if word_count[i] >= 3:
-		term_tf_term.append(i)
-        term_tf_freq.append(word_count[i])
-
-term_tf_list = list(zip(term_tf_term, term_tf_freq))
-# print(term_tf_list)
+for i in word_count_sorted:
+    if word_count[i] >= 25:
+        print(i, word_count[i])
 '''
-
 
 # create a list of "weighted term frequency(wtf)"
 # sort by word_count_sorted
 term_wtf_list = []
-for t in word_count_clean:
+for t in word_count_sorted:
     # certain term's score in certain article
     term_certain_tmp_list = []
     # always reset to zero as we loop through another term
@@ -127,7 +120,7 @@ for t in word_count_clean:
 
 # create a clean and sorted list of unique terms(sorted by wtf)
 term_sorted_list = [] 
-for i in word_count_clean:
+for i in word_count_sorted:
     term_sorted_list.append(i)
 
 # 馬英九：25071, 柯文哲：3196, 的：337202
@@ -137,11 +130,11 @@ term_final_list = sorted(term_wtf_combined, key=lambda x: x[1], reverse=True) # 
 
 # export the dataframe as csv!
 final_data = pd.DataFrame(term_final_list, columns=['term', 'wtf'])
-final_data.to_csv('FB_WTF_0804_NewsDeep.csv', header=True)
+final_data.to_csv('FB_WTF_08190825_food.csv', header=True)
 term2wtf = final_data.set_index('term')['wtf'].to_dict()
 # import ipdb; ipdb.set_trace()
 
-'''
+
 #================article ranking starts here================
 
 term_article_term = []
@@ -162,13 +155,13 @@ topfive = list(zip(term_article_term, term_article_wtf, term_article_content, te
 topfive_with_duplicate = pd.DataFrame(topfive, columns=['term', 'term_wtf', 'content', 'article_score'])
 topfive_with_duplicate_sort = topfive_with_duplicate.sort_values(by=['term_wtf', 'article_score'], ascending=False)
 topfive_with_duplicate_sort.index = np.arange(topfive_with_duplicate_sort.shape[0])
-topfive_with_duplicate_result = topfive_with_duplicate_sort.groupby('term', sort=False).apply(lambda x: x[(x.index - x.index.min()) < 5])
+topfive_with_duplicate_result = topfive_with_duplicate_sort.groupby('term', sort=False).apply(lambda x: x[(x.index - x.index.min()) < 3])
 
-topfive_with_duplicate_result.to_csv('FB_top5all_0805_Politics.csv')
+topfive_with_duplicate_result.to_csv('FB_5foreach_08190825_food.csv')
 
-
+'''
 # top 5 article/per term; drop duplicated articles
 topfive_no_duplicate = topfive_with_duplicate.drop_duplicates(subset=['content'], keep='first')
 topfive_no_duplicate_sort = topfive_no_duplicate.sort_values(by=['term_wtf', 'article_score'], ascending=False)
-topfive_no_duplicate_sort.to_csv('FB_top5clean_0805_Politics.csv')
+topfive_no_duplicate_sort.to_csv('FB_top20_08190825_food.csv')
 '''
